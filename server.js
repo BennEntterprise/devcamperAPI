b8d04d578d5d7f81dbfd9f768312708f7cc39c16
BennEntterprise <kyle@BennEntterprise.com>
2019-10-18 12:07:04 -0400

    implement morgan as logger middleware env=dev

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const logger = require('./middleware/logger')
+const morgan = require('morgan')
 //Route Files
 const bootcamps = require('./routes/bootcamps')
 
-// Load env vars
+// Load/Analyze Environment Variables
 dotenv.config({ path: './config/config.env' })
-
-//Initailize app. 
-const app = express();
-
-//Applying Middleware
-app.use(logger);
-
-//Mount Routers
-app.use('/api/v1/bootcamps', bootcamps);
-
 const PORT = process.env.PORT || 5000;
 let env;
 if (process.env.NODE_ENV === 'production') {
@@ -23,6 +13,18 @@ if (process.env.NODE_ENV === 'production') {
 } else {
     env = 'development'
 }
+
+//Initailize app. 
+const app = express();
+
+//Dev Loggin Middleware
+if (env === 'development') {
+    app.use(morgan('dev'))
+}
+
+//Mount Routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 
 app.listen(PORT, console.log(`Server running in ${
     process.env.NODE_ENV
