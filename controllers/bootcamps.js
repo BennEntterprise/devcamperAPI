0c0d988c3eb40f4c6c55be01c779a89f5d7b3806
BennEntterprise <kyle@BennEntterprise.com>
2019-10-18 18:09:56 -0400

    create delete/update routes. (with basic errhandling)
    err hanlding is done using try catch in each rotue.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,7 +9,7 @@ const Bootcamp = require('../models/Bootcamp')
 exports.getBootcamps = async (req, res, next) => {
     try {
         const bootcamps = await Bootcamp.find();
-        res.status(200).json({ success: true, data: bootcamps })
+        res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps })
     } catch (err) {
         res.status(400).json({ success: false, msg: 'bad request' })
     }
@@ -50,17 +50,33 @@ exports.createBootcamp = async (req, res, next) => {
 // @desc    Update Single Bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private 
-exports.updateBootcamp = (req, res, next) => {
-    res
-        .status(200)
-        .json({ success: true, msg: `Update Bootcamp ${req.params.id}` })
+exports.updateBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+        if (!bootcamp) {
+            return res.status(400).json({ success: false })
+        }
+        res.status(200).json({ sucess: true, data: bootcamp })
+    } catch (err) {
+        res.status(400).json({ success: false, msg: err.message })
+    }
 }
+
 
 // @desc    Delete Single Bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private 
-exports.deleteBootcamp = (req, res, next) => {
-    res
-        .status(200)
-        .json({ success: true, msg: `Delete Bootcamp ${req.params.id}` })
+exports.deleteBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
+        if (!bootcamp) {
+            return res.status(400).json({ success: false })
+        }
+        res.status(200).json({ sucess: true, data: {} })
+    } catch (err) {
+        res.status(400).json({ success: false, msg: err.message })
+    }
 }
