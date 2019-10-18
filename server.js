3605fedb47ba5def6987553475dd53b963d2a527
BennEntterprise <kyle@BennEntterprise.com>
2019-10-18 11:31:53 -0400

    add basic bootcamp routes.

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
+//Route Files
+const bootcamps = require('./routes/bootcamps')
 
 // Load env vars
-dotenv.config({path: './config/config.env'})
+dotenv.config({ path: './config/config.env' })
 
 
 const app = express();
+
+//Mount Routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 let env;
@@ -18,4 +23,4 @@ if (process.env.NODE_ENV === 'production') {
 
 app.listen(PORT, console.log(`Server running in ${
     process.env.NODE_ENV
-} on port ${PORT}`))
+    } on port ${PORT}`))
