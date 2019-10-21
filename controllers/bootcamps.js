8de7ac3015ce747c01ab5595f969f518a5250fea
BennEntterprise <kyle@BennEntterprise.com>
2019-10-21 16:11:43 -0400

    create courses seeder and course mongoosemodel

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -12,7 +12,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     const reqQuery = { ...req.query }
 
     //Fields to Exclude from query
-    const removeFields = ['select', 'sort']
+    const removeFields = ['select', 'sort', 'page', 'limit']
     //Loop over remove fields, delete from reqQuery
     removeFields.forEach(param => delete reqQuery[param])
 
@@ -39,10 +39,33 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
         query = query.sort('-createdAt')
     }
 
+    //pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 1;
+    const startIndex = (page - 1) * limit;
+    const endIndex = (page * limit)
+    const total = await Bootcamp.countDocuments();
+
+    query = query.skip(startIndex).limit(limit)
+
     //Execute Query
     const bootcamps = await query
-    console.log(bootcamps)
-    res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps })
+    //Pagination Result
+    const pagination = {};
+    if (endIndex < total) {
+        pagination.next = {
+            page: page + 1,
+            limit: limit
+        }
+    }
+    if (startIndex > 0) {
+        pagination.prev = {
+            page: page - 1,
+            limit: limit
+        }
+    }
+
+    res.status(200).json({ success: true, pagination, count: bootcamps.length, data: bootcamps })
 })
 
 // @desc    Get Single Bootcamp
