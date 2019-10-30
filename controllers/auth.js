887efb5cfbd2795c716fd4e9952916090f18caf0
BennEntterprise <kyle@bennentterprise.com>
2019-10-30 14:16:16 -0400

    missing gile.

--- controllers/auth.js
+++ controllers/auth.js
@@ -55,16 +55,19 @@ exports.login = asyncHandler(async (req, res, next)=>{
  //Get Token from Model, create cookie and send response. 
  const sendTokenResponse = (user, statusCode, res ) =>{
     const options = {
-        expires: new Date(Date.now + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true
     }
 
+    if(process.env.NODE_ENV === 'production'){
+        options.secure = true
+    }
      //Create token
      const token = user.getSignedJwtToken(); 
  
      res
         .status(statusCode)
-        .cookie('token', token, optionsS)
+        .cookie('token', token, options)
         .json({
             sucess: true, 
             token: token
